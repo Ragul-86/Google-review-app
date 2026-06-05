ba8838983b8c9d0e98d6876069d2d36d356acff4
Ragul B <139948419+Ragul-86@users.noreply.github.com>
2026-06-05 17:51:39 +0530

    update

--- google-review/src/pages/AdminLogin.jsx
+++ google-review/src/pages/AdminLogin.jsx
@@ -20,7 +20,7 @@ function AdminLogin() {
     try {
       const res =
         await axios.post(
-          "http://localhost:5000/api/auth/login",
+          "https://google-review-app-wi5e.onrender.com/api/auth/login",
           {
             email,
             password,
